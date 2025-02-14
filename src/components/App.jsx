3fc102c34cb0181bbc52c57aa5acf0fa3ae6f3eb
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-14 17:36:33 +0100

    feat: Added redirect to new portfolio site in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,26 @@
-import Navbar from "./navbar/Navbar";
-import Header from "./header/Header";
-import AboutMe from "./aboutMe/AboutMe";
-import Skills from "./skills/Skills";
-import Projects from "./projects/Projects";
-import Footer from "./footer/Footer";
+// import Navbar from "./navbar/Navbar";
+// import Header from "./header/Header";
+// import AboutMe from "./aboutMe/AboutMe";
+// import Skills from "./skills/Skills";
+// import Projects from "./projects/Projects";
+// import Footer from "./footer/Footer";
+import { useEffect } from "react";
 
 export default function App() {
-    return (
-        <>
-            <Navbar />
-            <Header />
-            <AboutMe />
-            <Skills />
-            <Projects />
-            <Footer />
-        </>
-    );
+    useEffect(() => {
+        window.location.href = "https://lakiolive-portfolio.vercel.app/";
+    }, []);
+
+    return null;
+
+    // return (
+    //     <>
+    //         <Navbar />
+    //         <Header />
+    //         <AboutMe />
+    //         <Skills />
+    //         <Projects />
+    //         <Footer />
+    //     </>
+    // );
 }
